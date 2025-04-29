892b81ada47ee618e4e6138bdcddd05f72a010df
Nilakshisarode <152787266+Nilakshisarode@users.noreply.github.com>
2025-04-29 17:58:48 +0530

    Add files via upload

---
+++ secure_c.py
@@ -0,0 +1,20 @@
+import cv2
+import winsound 
+webcam =cv2.VideoCapture(0)
+while True:
+    _,img1=webcam.read()
+    _,img2=webcam.read()
+    diff =cv2.absdiff(img1,img2)
+    gray=cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Security camera",gray)
+    _,thresh=cv2.threshold(gray,20,255,cv2.THRESH_BINARY)
+    contours,_=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        if cv2.contourArea(c) < 1000 :
+            continue
+        winsound.Beep(500,100)
+    if cv2.waitKey(10) == 27:
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
